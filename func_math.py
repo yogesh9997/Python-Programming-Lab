b02d203964ea8fb80cf675aa6dd3bd793337a153
YOGESH SHARMA <100750699+yogesh9997@users.noreply.github.com>
2022-05-31 10:30:46 +0530

    Create func_math.py

---
+++ func_math.py
@@ -0,0 +1,15 @@
+def fun(a,b):
+    s=0
+    sum=a+b
+    dif=a-b
+    mul=a*b
+    div=a//b
+    mod=a%b
+    print("Sum is :",sum)
+    print("Diff is :",dif)
+    print("Multi is :",mul)
+    print("Div is : ",div)
+    print("Mod is  :",mod)
+p=int(input("enter 1st no."))
+q=int(input("enter 2nd no."))
+fun(p,q)
