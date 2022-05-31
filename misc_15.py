348857dacb216e1210b85300be58767a32e8ab04
YOGESH SHARMA <100750699+yogesh9997@users.noreply.github.com>
2022-05-31 10:51:44 +0530

    Create misc_15.py

---
+++ misc_15.py
@@ -0,0 +1,9 @@
+#find the  the greatest common divisor of the two integers
+
+import math
+
+print (math.gcd(3, 6))
+print (math.gcd(6, 12))
+print (math.gcd(12, 36))
+print (math.gcd(-12, -36))
+print (math.gcd(5, 12))
