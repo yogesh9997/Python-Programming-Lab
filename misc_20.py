26b66724b1020cc2d0804c34e91f23978f82c853
YOGESH SHARMA <100750699+yogesh9997@users.noreply.github.com>
2022-05-31 10:58:13 +0530

    Create misc_20.py

---
+++ misc_20.py
@@ -0,0 +1,10 @@
+
+# Return the sine of different values
+
+import math
+
+print (math.sin(0.00))
+print (math.sin(-1.23))
+print (math.sin(10))
+print (math.sin(math.pi))
+print (math.sin(math.pi/2))
