187078157932ad0918f73e3d5457696d1041d11f
YOGESH SHARMA <100750699+yogesh9997@users.noreply.github.com>
2022-05-31 10:29:11 +0530

    Create func_factorial.py

---
+++ func_factorial.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Input a number to compute the factiorial : "))
+print(factorial(n))
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Secur
