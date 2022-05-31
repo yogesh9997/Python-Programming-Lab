ff9a106cb1e6c928f5c2d2e611fefda179573944
YOGESH SHARMA <100750699+yogesh9997@users.noreply.github.com>
2022-05-31 11:11:27 +0530

    Create square_dict.py

---
+++ square_dict.py
@@ -0,0 +1,10 @@
+n=int(input("enter the range till u want to print the squares and doubles"))
+sq={x:x*x for x in range(1,n+1)}
+print(sq)
+print("\n")
+do={x:2*x for x in range(1,n+1)}
+print(do)
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
