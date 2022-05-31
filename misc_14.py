944632a34b816f6a228967cd8ef0a6747994f291
YOGESH SHARMA <100750699+yogesh9997@users.noreply.github.com>
2022-05-31 10:50:11 +0530

    Create misc_14.py

---
+++ misc_14.py
@@ -0,0 +1,13 @@
+import math
+
+# Print the square root of different numbers
+print (math.sqrt(10))
+print (math.sqrt (12))
+print (math.sqrt (68))
+print (math.sqrt (100))
+
+# Round square root downward to the nearest integer
+print (math.isqrt(10))
+print (math.isqrt (12))
+print (math.isqrt (68))
+print (math.isqrt (100))
